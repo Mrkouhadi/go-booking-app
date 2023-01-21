4a08526cd0889fc6edf6addf0abe0aced0ee4387
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2023-01-21 22:14:28 +0800

    Initial Commit

---
+++ cmd/web/routes.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/mrkouhadi/go-booking-app/pkg/config"
+	"github.com/mrkouhadi/go-booking-app/pkg/handlers"
+)
+
+func Routes(app *config.AppConfig) http.Handler {
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Use(Nosurf)
+	mux.Use(LoadSession)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/features", handlers.Repo.Features)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
