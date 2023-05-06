7e10d5d73cd2f950d8878b6fd1d1e1361c7f9aa5
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2023-05-07 00:54:10 +0800

    feat: added info logger and error logger

---
+++ internal/helpers/helpers.go
@@ -0,0 +1,29 @@
+package helpers
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+
+	"github.com/mrkouhadi/go-booking-app/internal/config"
+)
+
+var app *config.AppConfig
+
+// Newhelpers sets up appconfig for helpers
+func Newhelpers(a *config.AppConfig) {
+	app = a
+}
+
+// ClientError sends errors to the client when something goes wrong on the clientside
+func ClientError(w http.ResponseWriter, status int) {
+	app.InfoLog.Println("Client error with status of ", status)
+	http.Error(w, http.StatusText(status), status)
+}
+
+// ServerError sends errors to the client when something goes wrong on the serverside
+func ServerError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.ErrorLog.Println(trace)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
