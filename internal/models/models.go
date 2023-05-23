309ca52ecfab28bb72f027098329b423221dc211
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2023-05-23 22:38:24 +0800

    feat: added more models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,61 @@
 package models
 
-// Reservation holds data of Reservation
+import "time"
+
+// User is the user model
+type User struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// Room is the Room model
+type Room struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Restriction is the restriction model
+type Restriction struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+// reservation is the model of reservation
 type Reservation struct {
+	ID        int
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomId    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Room // optional
+}
+
+// RoomRestrictions is the model of room_restriction
+type RoomRestrictions struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomId        int
+	ReservationId int
+	RestrictionId int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Room        // optional
+	Reservation   Reservation // optional
+	Restriction   Restriction // optional
 }
