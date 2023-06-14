fc5c4d2c8d43fa4dd46761dcf92659cc11c8acb3
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2023-06-14 23:55:54 +0800

    feat: added seeding to rooms and restrictions tables

---
+++ migrations/20230614154909_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('Generals Quarters','2023-02-18 00:00:00.000','2023-02-18 00:00:00.000'),
+	 ('Major Suite','2023-02-18 00:00:00.000','2023-05-29 00:00:00.000');
